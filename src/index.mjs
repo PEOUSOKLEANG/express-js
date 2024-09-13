ce91f62cb4f00a29cc6c0d31700d2750463f28c2
PEOUSOKLEANG <sokleang34802@gmail.com>
2024-09-13 16:15:17 +0700

    method:post,put,read,delete

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json())
 const mockUser = [
     {id:1 , username:"sokleang" , displayname:"sokleang"},
     {id:2 , username:"sokkim" , displayname:"sokkim"},
@@ -14,13 +15,18 @@ const PORT = process.env.PORT || 3004;
 app.get("/",(request,response)=>{
     response.status(201).send({msg:`Hello`})
 })
+
+// this include filter 
 app.get("/api/users",(request,response)=>{
     console.log(request.query);
     const {query:{filter , value},} = request;
-    
-    // filter and value is underfined
-    if(!filter &&! value) return response.status(201).send(mockUser);
+    if(filter&&value) return response.send(
+        mockUser.filter((user)=>user[filter].includes(value))
+    );
+    return response.send(mockUser);
 })
+
+//params users 
 app.get("/api/users/:id",(request,response)=>{
     // response.status(201).send(mockUser);
     const parsedId = parseInt(request.params.id);
@@ -28,7 +34,6 @@ app.get("/api/users/:id",(request,response)=>{
     if(isNaN(parsedId)) return response.status(400).send({msg:`Bad Request. Invalid Id`});
 
     const findUser = mockUser.find((user)=>user.id === parsedId);
-    // if(!findUser) return response.send(`User not found`);
     if(!findUser) return response.sendStatus(404);
     return response.send(findUser);
 
@@ -43,6 +48,39 @@ app.get('/api/products',(request, response)=>{
         {id:1532 , name:"iphone16pro", price:1600 },
         {id:2532 , name:"iphone16mini", price:1600 },
     ])
+})
+
+//method Post 
+app.post('/api/users',(request, response)=>{
+    console.log(request.body);
+    const{body} = request;
+    const newUser = {id:mockUser[mockUser.length-1].id+1,...body}
+    mockUser.push(newUser);
+    return response.status(201).send(newUser);
+})
+
+// method patch 
+app.put('api/users/:id',(request,response)=>{
+    const {body , params:{id}}=request;
+    const parsedId = parseInt(id);
+    if(isNaN(parsedId)) return response.sendStatus(400);
+    const findUserIndex = mockUser.findIndex((user)=>user.id === parsedId);
+
+    if(findUserIndex ===-1) return response.sendStatus(404);
+    mockUser[findUserIndex] = {id:parsedId , ...body};
+    return response.sendStatus(200);
+})
+
+//method delete 
+app.delete('/api/users/:id',(request, response)=>{
+    const { params:{id}}=request;
+    const parsedId = parseInt(id);
+    if(isNaN(parsedId)) return response.sendStatus(400);
+    const findUserIndex = mockUser.findIndex((user)=>user.id === parsedId);
+    if(findUserIndex ===-1) return response.sendStatus(404);
+    mockUser.splice(findUserIndex ,1)
+    //1 the number count to delete if no it will start from input to all number 
+    return response.sendStatus(200);
 })
 
 app.listen(PORT ,()=>{
